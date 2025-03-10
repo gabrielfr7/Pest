bb5c5ade8b4e86ba91d92f0535e29c68369ceaad
Freire <freire.anjos08@aluno.ifce.edu.br>
2025-03-10 16:22:47 -0300

    Create q1.py

---
+++ Exercicios/q1.py
@@ -0,0 +1,12 @@
+# 1.Verificar se o numero é impar ou par
+num = int(input("digite o numero:"))
+
+if num == 0:
+    print("neutro")
+
+elif num % 2 == 0:
+    print("par")
+
+else:
+    print("impar")
+    
