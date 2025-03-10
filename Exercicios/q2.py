d4912bc1e6eff16e359aa05c8d6f60630ea4a3fb
Freire <freire.anjos08@aluno.ifce.edu.br>
2025-03-10 16:24:08 -0300

    Create q2.py

---
+++ Exercicios/q2.py
@@ -0,0 +1,15 @@
+# 2. verificação de nota
+
+nota = float(input("digite a nota do aluno:"))
+
+if nota >= 9:
+    print("A")
+
+elif 7 <= nota < 9:
+    print("B")
+
+elif 5 <= nota < 7:
+    print("C")
+
+else:
+    print("D")
