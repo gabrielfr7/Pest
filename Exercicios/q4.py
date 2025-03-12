76aa7f11deca6b0cff2c8379caa8681f2144069e
Freire <freire.anjos08@aluno.ifce.edu.br>
2025-03-12 19:34:15 -0300

    Create q4.py

---
+++ Exercicios/q4.py
@@ -0,0 +1,12 @@
+# 4. soma de 3 numeros
+
+num1 = int(input("digite um numero inteiro:"))
+num2 = int(input("digite um numero inteiro:"))
+num3 = int(input("digite um numero inteiro:"))
+
+
+if num1 == num2 or num1 == num3 or num2 == num3:
+    print(0)
+else:
+    print(num1 + num2 + num3)
+
