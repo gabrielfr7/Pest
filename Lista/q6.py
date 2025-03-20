6b5bcc76df4a79b4925f23798445bd8b9801c691
Gabriel Freire <freire.anjos08@aluno.ifce.edu.br>
2025-03-20 19:49:18 -0300

    Update q6.py

--- Lista/q6.py
+++ Lista/q6.py
@@ -1,1 +1,22 @@
+ # 6. Programa para adivinhar o numero dado pelo o usuario
+
+import random
+
+# Gera um número aleatório entre 1 e 9
+numero_aleatorio = random.randint(1, 9)
+
+tentativa = int(input("digite sua tentativa:"))
+
+while True:
+    if numero_aleatorio == "sair":
+     print("o jogo acabou!")
+    elif tentativa < numero_aleatorio:
+       print("muito baixo!")
+       tentativa = int(input("digite sua tentativa:"))
+    elif tentativa > numero_aleatorio:
+       print("muito alto!")
+       tentativa = int(input("digite sua tentativa:"))
+    else:
+       print("parabens, você acertou")
+       break     
 
