73e55b4783687d40c9334178700a7fe6b37b2549
Gabriel Freire <freire.anjos08@aluno.ifce.edu.br>
2025-03-21 11:06:58 -0300

    Update q4.py

--- Lista/q4.py
+++ Lista/q4.py
@@ -1,1 +1,9 @@
+N = int(input("digite um numero:"))
+# N = 10
 
+contador = 1 
+
+while contador <= N:
+    if(N % contador == 0): 
+        print(f"{contador} é divisor")
+    contador = contador + 1
